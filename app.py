60b2895bab9b4a3479670ebac50c62547c91f6cf
Basma-Arnaoui <basma.arnaoui@gmail.com>
2024-04-30 16:41:45 +0100

    generate

--- app.py
+++ app.py
@@ -17,11 +17,12 @@ def menu():
 
 def generate_random_processes(num_processes, arrival_range, burst_range, priority_range=None):
     processes = []
-    for _ in range(num_processes):
+    for i in range(num_processes):
         arrival_time = random.randint(*map(int, arrival_range.split(',')))
         burst_time = random.randint(*map(int, burst_range.split(',')))
         priority = random.randint(*map(int, priority_range.split(','))) if priority_range else None
         process = {
+            'process_id' : i+1,
             'arrival_time': arrival_time,
             'burst_time': burst_time,
         }
@@ -33,14 +34,12 @@ def generate_random_processes(num_processes, arrival_range, burst_range, priorit
 
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
-    if request.method == 'POST':
         num_processes = int(request.form['num_processes'])
         arrival_range = request.form['arrival_time_range']
         burst_range = request.form['burst_time_range']
         priority_range = request.form['priority_range'] if 'priority_range' in request.form else None
         processes = generate_random_processes(num_processes, arrival_range, burst_range, priority_range)
-        return render_template('entered_processes.html', processes=processes)
-    return render_template('generate.html')
+        return render_template('generated_processes.html', processes=processes)
 
 @app.route('/upload')
 def upload():
